dc116d2d47e38cef63b82a0934128f65c0cdaf3c
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-05-11 09:40:52 -0700

    added pages directory with an index file for testing

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <ClientOnly>
+    <div>
+      <h1 class="bg-blue-300">Hero Home Care</h1>
+      <p class="bg-">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti
+        necessitatibus tempora obcaecati laboriosam quam excepturi est vitae
+        iusto voluptas placeat? Voluptates veritatis distinctio laboriosam
+        repudiandae nulla, quaerat commodi maxime nam!
+      </p>
+    </div>
+  </ClientOnly>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
