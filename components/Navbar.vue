2c2edd76a6e8c747397a19f85e3942fcf3fba906
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-05-11 10:39:59 -0700

    added components navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav>
+      <div>
+        <NuxtLink to="/">Home</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink to="/About">How It Works</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink to="/">Home</NuxtLink>
+      </div>
+      <div></div>
+      <div></div>
+    </nav>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
