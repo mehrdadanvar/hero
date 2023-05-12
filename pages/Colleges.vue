1ac06b276b5e1d128f56021b4151f6e4237ea67d
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-05-12 08:39:39 -0700

    added a new page to draw potential colleges for cooperation

---
+++ pages/Colleges.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      <a href="https://www.douglascollege.ca/program/cthca" target="_blank"
+        >Douglas College</a
+      >
+    </div>
+    <div>
+      <a
+        href="https://colleges-bc.ca/healthcare/practical-nursing"
+        target="_blank"
+        >Vancouver Career College</a
+      >
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
